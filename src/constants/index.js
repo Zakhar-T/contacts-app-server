e30bea44166150b9b4ab6321f2fa3dbd76bce657
Zakhar-T <zakhar.null@gmail.com>
2025-08-05 12:05:41 +0300

    edit dir creation func

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,8 +16,8 @@ export const SMTP = {
   SMTP_FROM: 'SMTP_FROM',
 };
 
-export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
-export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
+export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'src/temp');
+export const UPLOAD_DIR = path.join(process.cwd(), 'src/uploads');
 
 export const CLOUDINARY = {
   CLOUD_NAME: 'CLOUD_NAME',
--- src/utils/createDirIfNotExists.js
+++ src/utils/createDirIfNotExists.js
@@ -1,9 +1,13 @@
 import fs from 'node:fs/promises';
 
 export async function createDirIfNotExists(url) {
+  console.log(url);
+
   try {
-    fs.access(url);
+    await fs.access(url);
   } catch (error) {
-    if (error.code === 'ENOENT') await fs.mkdir(url);
+    if (error.code === 'ENOENT') {
+      await fs.mkdir(url);
+    }
   }
 }
